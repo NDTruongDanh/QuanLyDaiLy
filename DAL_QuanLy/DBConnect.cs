c7904dae23549708a2a1d9a1afa17b2d8c4a0e77
Xuan-Hien Dieu <79910980+dieuxuanhien@users.noreply.github.com>
2025-05-16 13:45:41 +0700

    dbconnect

---
+++ DAL_QuanLy/DBConnect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Microsoft.Data.SqlClient;
+
+namespace DAL_QuanLy
+{
+    public class DBConnect
+    {
+        protected SqlConnection _conn = new SqlConnection("Data Source=localhost;Initial Catalog=QUANLYDAILY;Integrated Security=True;TrustServerCertificate=True");
+    }
+}
